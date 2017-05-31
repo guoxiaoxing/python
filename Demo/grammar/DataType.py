50203bca8f8095720cbdc5934ae8eba009cf28ca
guoxiaoxing <guoxiaoxingse@gmail.com>
2017-05-31 17:13:14 +0800

    add demo

---
+++ Demo/grammar/DataType.py
@@ -0,0 +1,22 @@
+print("\\\\\\")
+print(r"\\\\\\")
+print("line1\nline2")
+print('''line1 ... line2 ... line3''')
+print(10 / 3)
+print(10 / 2)
+print(10 // 2)
+print(10 % 2)
+print(ord("c"))
+
+# str
+
+a = "abc"
+print(a.encode("utf-8"))
+
+print("%d is a five" % 5)
+print("%2d %02d" % (3, 1))
+
+# list
+colorList = ['green', 'red', 'yellow']
+print('len(colorList) : ' + len(colorList).__str__())
+print('colorList.append('blue') : ' + colorList.append('blue').__str__())
